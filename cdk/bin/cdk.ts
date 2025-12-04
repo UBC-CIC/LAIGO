30d236da30ca8a63cecf038ee7684a25715df3e0
Aniket Nemade <anunem2@gmail.com>
2025-12-04 15:35:20 -0800

    vpc stack init

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { VpcStack } from '../lib/vpc-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Parse params from command line 
+const StackPrefix = app.node.tryGetContext('StackPrefix');
+const version = app.node.tryGetContext('Version');
+const githubRepo = app.node.tryGetContext('GithubRepo');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// grab account and region info 
+const env: cdk.Environment = { 
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION 
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+
+
+const vpc = new VpcStack(app, `${StackPrefix}-VpcStack`, {env, stackPrefix: StackPrefix});
+
+
